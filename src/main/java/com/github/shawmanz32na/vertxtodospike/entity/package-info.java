09a0e4942417e342c9a98f4bfe3454f0f359aad3
Kevin Shaw <shaw@g2ss.com>
2018-08-14 11:43:36 -0700

    Step 1 - Todo entity
    Includes Maven project skeleton.

---
+++ src/main/java/com/github/shawmanz32na/vertxtodospike/entity/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Indicates that this module contains classes that need to be generated /
+ * processed.
+ */
+@ModuleGen(name = "vertx-todo-entity", groupPackage = "com.github.shawmanz32na.vertxtodospike.entity")
+package com.github.shawmanz32na.vertxtodospike.entity;
+
+import io.vertx.codegen.annotations.ModuleGen;
